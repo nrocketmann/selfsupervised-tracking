a60d2a5e95f539fbc9639c76308b6f5c5618bc35
nrocketmann <nameerhirschkind@gmail.com>
2021-11-22 11:17:21 -0500

    small fixes

--- gl_scripts/train_nndustbin-scratch.sh
+++ gl_scripts/train_nndustbin-scratch.sh
@@ -8,16 +8,16 @@
 #SBATCH --mail-user=nameer@umich.edu
 #SBATCH --job-name=train-nndustbin
 #SBATCH --account=eecs542f21_class
-cd ~/EECS42
+cd ~/EECS542
 module load python/3.8.7
 source crw_env/bin/activate
 export TRAIN_PY="selfsupervised-tracking/code/train.py"
-export DATA_PATH="clips/"
+export DATA_PATH="vos_clips/"
 export CACHE_PATH="vos_all_clips"
 export OUTPUT_PATH="nndustbin_scratch1/"
 mkdir $OUTPUT_PATH
 python -u -W ignore $TRAIN_PY --data-path $DATA_PATH \
         --frame-aug grid --dropout 0.1 --clip-len 4 --temp 0.05 \
         --model-type scratch  --workers 32 --batch-size 12 \
-        --load-cache $CACHE_PATH --data-parallel --lr 0.0001 \
-        --output-dir $OUTPUT_PATH --epochs 200 --lr-milestones 250 \
+        --save-cache $CACHE_PATH --data-parallel --lr 0.0001 \
+        --output-dir $OUTPUT_PATH --epochs 200 --lr-milestones 250
